c6518b829744c8b5f14e348f7890cd850832cdb8
davspas <davidsmith577@gmail.com>
2017-04-05 21:44:13 -0300

    Atualizada versão inicial do projeto com câmeras de dois personagens, sprites de mockup e inicio do código de movimento

---
+++ Viking's Olympics - Violent Paddling/Assets/Scripts/TesteMovimento.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TesteMovimento : MonoBehaviour {
+
+	public float qtde = 1000.0f;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float h = Input.GetAxis ("Horizontal") * qtde * Time.deltaTime;
+
+	}
+}
