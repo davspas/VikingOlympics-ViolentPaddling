a22628215d5abc3700c5a0eed04cacac70128ca7
davspas <davidsmith577@gmail.com>
2017-04-26 20:42:45 -0300

    organização de arquivos

---
+++ Viking's Olympics - Violent Paddling/Assets/Scripts/Timer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	public Text textTimer;
+    private float tempoDecorrido;
+ 
+	void Update() {
+		tempoDecorrido += Time.deltaTime;
+
+		float minutos = tempoDecorrido / 60;
+		float segundos = tempoDecorrido % 60;
+		float fracao = (tempoDecorrido * 100) % 100;
+
+		textTimer.text = string.Format ("{0:00}:{1:00}:{2:000}", minutos, segundos, fracao);
+	}
+}
